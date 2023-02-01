66e52bcb6e7d0681207421acb009ea885e7604b0
SerhiiKutovyi <wagenrex@gmail.com>
2023-02-01 17:01:21 +0200

    componentDidUpdat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,103 +2,93 @@ import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 import { Loader } from './Loader/Loader';
 
-// import axios from 'axios';
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-
 const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export class App extends Component {
-  state = {
-    articles: [],
-    search: '',
-    page: 1,
-    loading: false,
-    image: '',
-  };
+export const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState('');
 
-  componentDidMount() {
-    window.addEventListener('keydown', eve => {
-      if (eve.code === 'Escape') {
-        this.setState({ image: '' });
-      }
-    });
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { search, page } = this.state;
-    if (prevState.search !== search || prevState.page !== page) {
-      this.setState({ loading: true });
-      fetch(
-        `${BASE_URL}?q=${search}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(res => res.json())
-        .then(cards => {
-          if (cards.hits.length < 1) {
-            toast.error('Enter a valid search term!');
-          }
-          this.setState(prevState => ({
-            articles: [...prevState.articles, ...cards.hits],
-          }));
-        })
-        .catch(error => console.log(error))
-        .finally(() => {
-          this.setState({ loading: false });
-        });
+  useEffect(() => {
+    if (search === '') {
+      return;
     }
-  }
+    setLoading(true);
+    fetch(
+      `${BASE_URL}?q=${search}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(cards => {
+        if (cards.hits.length < 1) {
+          toast.error('Enter a valid search term!');
+        }
+        setArticles(prevState => [...prevState, ...cards.hits]);
+      })
+      .catch(error => console.log(error))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [search, page]);
 
-  componentWillUnmount() {
+  useEffect(() => {
     window.addEventListener('keydown', eve => {
       if (eve.code === 'Escape') {
-        this.setState({ image: '' });
+        setImage('');
       }
     });
-  }
+    return () => {
+      window.addEventListener('keydown', eve => {
+        if (eve.code === 'Escape') {
+          setImage('');
+        }
+      });
+    };
+  }, []);
 
-  searchBarSubmit = data => {
-    this.setState({ search: data, page: 1, articles: [] });
+  const searchBarSubmit = data => {
+    setSearch(data);
+    setPage(1);
+    setArticles([]);
   };
 
-  clickLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const clickLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  showModalImg = largeImageURL => {
-    this.setState({ image: { largeImageURL } });
+  const showModalImg = largeImageURL => {
+    setImage({ largeImageURL });
   };
 
-  closeModalImg = eve => {
+  const closeModalImg = eve => {
     if (eve.currentTarget === eve.target) {
-      this.setState({ image: '' });
+      setImage('');
     }
   };
 
-  render() {
-    const { articles, image } = this.state;
-
-    return (
-      <AppStyles>
-        <Searchbar onSubmitHandler={this.searchBarSubmit} />
-        {articles.length <= 0 && <h1>Enter text</h1>}
-        {this.state.loading && <Loader />}
-        {articles.length > 0 ? (
-          <ImageGallery articles={articles} modalBigImg={this.showModalImg} />
-        ) : null}
-        {articles.length > 11 && <Button clickLoad={this.clickLoadMore} />}
-        {image && <Modal src={image} close={this.closeModalImg} />}
-        <ToastContainer autoClose={2000} />
-      </AppStyles>
-    );
-  }
-}
+  return (
+    <AppStyles>
+      <Searchbar onSubmitHandler={searchBarSubmit} />
+      {articles.length <= 0 && <h1>Enter text</h1>}
+      {loading && <Loader />}
+      {articles.length > 0 ? (
+        <ImageGallery articles={articles} modalBigImg={showModalImg} />
+      ) : null}
+      {articles.length > 11 && <Button clickLoad={clickLoadMore} />}
+      {image && <Modal src={image} close={closeModalImg} />}
+      <ToastContainer autoClose={2000} />
+    </AppStyles>
+  );
+};
 
 const AppStyles = styled.div`
   text-align: center;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,46 @@
 import PropTypes from 'prop-types';
 import { FcSearch } from 'react-icons/fc';
 import { toast } from 'react-toastify';
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Search } from './Searchbar.styles';
 
-export class Searchbar extends Component {
-  state = {
-    inputValue: '',
+export const Searchbar = ({ onSubmitHandler }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handelChange = eve => {
+    setInputValue(eve.target.value);
   };
 
-  handelChange = eve => {
-    this.setState({ inputValue: eve.target.value });
-  };
-
-  handleSubmit = eve => {
+  const handleSubmit = eve => {
     eve.preventDefault();
-    if (this.state.inputValue.trim() === '') {
+    if (inputValue.trim() === '') {
       toast.error('Fill in the search bar!');
       return;
     }
-    this.props.onSubmitHandler(this.state.inputValue);
+    onSubmitHandler(inputValue);
 
-    this.setState({ inputValue: '' });
+    setInputValue('');
   };
 
-  render() {
-    return (
-      <>
-        <Search>
-          <form onSubmit={this.handleSubmit}>
-            <button type="submit">
-              <span>{<FcSearch />}</span>
-            </button>
+  return (
+    <>
+      <Search>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">
+            <span>{<FcSearch />}</span>
+          </button>
 
-            <input
-              value={this.state.inputValue}
-              onChange={this.handelChange}
-              type="text"
-              placeholder="Search images and photos"
-            />
-          </form>
-        </Search>
-      </>
-    );
-  }
-}
+          <input
+            value={inputValue}
+            onChange={handelChange}
+            type="text"
+            placeholder="Search images and photos"
+          />
+        </form>
+      </Search>
+    </>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmitHandler: PropTypes.func.isRequired,
